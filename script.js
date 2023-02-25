7d257412f143033cf3a978b919917d58091748e3
AntwonTheDamaja <52683702+antwonthedamaja@users.noreply.github.com>
2023-02-24 23:06:44 -0500

    Major bugfixes; display now works as intended

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 const buttons = document.querySelectorAll(".buttons");
 const display = document.querySelector("#display");
 //variables
+let inputDisplay;
 let input1;
 let input2;
 let operand;
@@ -10,6 +11,7 @@ buttons.forEach(button => {
     button.addEventListener('click', () => {
         if (button.id === 'clear') { //clear
             display.textContent = "000000";
+            inputDisplay = undefined;
             input1 = undefined;
             input2 = undefined;
             operand = undefined;
@@ -17,25 +19,30 @@ buttons.forEach(button => {
             operate();
             operand = undefined;
         } else if ((button.id === 'equals' && !operand) //do nothing / bugfix line
-            || (input1 === undefined && button.className === 'buttons operand')
-            || (operand && button.className === 'buttons operand')) {
+            || (inputDisplay === undefined && button.className === 'buttons operand')
+            || (input2 === undefined && operand && button.className === 'buttons operand')) {
             //click equals, no operand; click operand, no input; operand twice
         } else if (button.className === 'buttons operand' && !operand) { //click 1st operand
+            input1 = display.textContent;
             operand = button.id;
-            display.textContent += button.textContent;
+            inputDisplay += button.textContent;
+            display.textContent = inputDisplay;
         } else if (button.className === 'buttons operand' && operand) { //click 2nd operand
             operate();
             operand = button.id;
-            display.textContent += operand;
+            inputDisplay += button.textContent;
+            display.textContent = inputDisplay;
         } else if (!operand) { //click a number/decimal point without an operand
-            input1 === undefined ? input1 = button.textContent : input1 += button.textContent;
-            display.textContent = input1;
-        } else if (input2 === undefined) { //click a number/decimal point after 1st operand
+            inputDisplay === undefined ? inputDisplay = button.textContent : inputDisplay += button.textContent;
+            display.textContent = inputDisplay;
+        } else if (input2 === undefined) { //click a number/decimal after 1st operand, 1st iteration only
             input2 = button.textContent;
-            display.textContent += input2;
-        } else { //the problem is here
-            input2 += button.textContent
-            display.textContent += input2;
+            inputDisplay += button.textContent;
+            display.textContent = inputDisplay;
+        } else { //click a number/decimal after 1st operand
+            input2 += button.textContent;
+            inputDisplay += button.textContent;
+            display.textContent = inputDisplay;
         }
     });
 });
@@ -43,22 +50,24 @@ buttons.forEach(button => {
 function operate() {
     let divide0;
     if (operand === "add") {
-        input1 = parseFloat(input1) + parseFloat(input2);
+        inputDisplay = parseFloat(input1) + parseFloat(input2);
     } else if (operand === "subtract") {
-        input1 = parseFloat(input1) - parseFloat(input2);
+        inputDisplay = parseFloat(input1) - parseFloat(input2);
     } else if (operand === "multiply") {
-        input1 = parseFloat(input1) * parseFloat(input2);
+        inputDisplay = parseFloat(input1) * parseFloat(input2);
     } else if (operand === "divide" && input2 === '0') {
         display.textContent = "NICE TRY!";
         divide0 = true;
     } else {
-        input1 = parseFloat(input1) / parseFloat(input2);
+        inputDisplay = parseFloat(input1) / parseFloat(input2);
     }
 
     if (!divide0) {
+    input1 = inputDisplay;
     input2 = undefined;
-    display.textContent = input1;
+    display.textContent = inputDisplay;
     } else {
+    inputDisplay = undefined;
     input1 = undefined;
     input2 = undefined;
     operand = undefined;
